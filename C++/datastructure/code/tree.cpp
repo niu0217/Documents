44dec781e365366995b3b0c052d108a045e5081d
niu0217 <1033856427@qq.com>
2024-01-17 11:38:18 +0800

    修改tree的代码

--- C++/datastructure/code/tree.cpp
+++ C++/datastructure/code/tree.cpp
@@ -1,80 +1,68 @@
 #include<iostream>
 
 struct TreeNode {
-    int value_;
-    TreeNode* left_;
-    TreeNode* right_;
-    TreeNode(int val): value_(val), left_(nullptr), right_(nullptr) { }
+    int value;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val): value(val), left(nullptr), right(nullptr) { }
 };
 
 class BinaryTree {
 public:
-    BinaryTree(): root_(nullptr) { }
-    ~BinaryTree();
+    BinaryTree(): root(nullptr) { }
+    ~BinaryTree() {
+        deleteAllNode();
+    }
 
-    //操作二叉树
-    void clear();  //删除所有节点
-    void insert(int value);  //插入节点
-    void inorder_traversal() const;  //中序遍历
-
-    //辅助函数
-    void clear_helper(TreeNode* root);  //递归删除所有节点
-    void insert_helper(TreeNode* root, int value);  //递归插入节点到合适的位置
-    void inorder_traversal_helper(TreeNode* root) const;  //递归进行中序遍历
-
+    void deleteAllNode();
+    void deleteAllNodeHelper(TreeNode* toDeleteRoot);
+    void insertNode(int value);
+    void insertNodeHelper(TreeNode* rootOfTree, int value);
+    void midorderTraversal() const;
+    void midorderTraversalHelper(TreeNode* rootOfTree) const;
 private:
-    TreeNode* root_;
+    TreeNode* root;
 };
 
-BinaryTree::~BinaryTree() {
-    clear();
+void BinaryTree::deleteAllNode() {
+    deleteAllNodeHelper(root);
+    root = nullptr;
 }
 
-void BinaryTree::clear() {
-    clear_helper(root_);
-    root_ = nullptr;
-}
-
-void BinaryTree::insert(int value) {
-    if(root_ == nullptr) {
-        root_ = new TreeNode(value);
-    }
-    else {
-        insert_helper(root_, value);
-    }
-}
-
-void BinaryTree::inorder_traversal() const {
-    inorder_traversal_helper(root_);
-    printf("\n");
-}
-
-//采用后序遍历（递归）的方式删除所有节点
-void BinaryTree::clear_helper(TreeNode* root) {
-    if(root == nullptr) {
+void BinaryTree::deleteAllNodeHelper(TreeNode* toDeleteRoot) {
+    if(toDeleteRoot == nullptr) {
         return;
     }
-    clear_helper(root->left_);
-    clear_helper(root->right_);
-    delete root;
-    root = nullptr;  //置空避免悬挂指针
+    deleteAllNodeHelper(toDeleteRoot->left);
+    deleteAllNodeHelper(toDeleteRoot->right);
+    delete toDeleteRoot;
+    toDeleteRoot = nullptr;  //置空避免悬挂指针
 }
 
-void BinaryTree::insert_helper(TreeNode* root, int value) {
-    if(value < root->value_) {
-        if(root->left_ == nullptr) {
-            root->left_ = new TreeNode(value);
+void BinaryTree::insertNode(int value) {
+    if(root == nullptr) {
+        root = new TreeNode(value);
+    }
+    else {
+        insertNodeHelper(root, value);
+    }
+}
+
+void BinaryTree::insertNodeHelper(TreeNode* rootOfTree, int value) {
+    if(value < rootOfTree->value) {
+        if(rootOfTree->left == nullptr) {
+            rootOfTree->left = new TreeNode(value);
         }
         else {
-            insert_helper(root->left_, value);
+            insertNodeHelper(rootOfTree->left, value);
         }
     }
-    else if(value > root->value_) {
-        if(root->right_ == nullptr) {
-            root->right_ = new TreeNode(value);
+    else if(value > rootOfTree->value) {
+        if(rootOfTree->right == nullptr) {
+            rootOfTree->right = new TreeNode(value);
         }
         else {
-            insert_helper(root->right_, value);
+            insertNodeHelper(rootOfTree->right, value);
         }
     }
     else {
@@ -83,23 +71,32 @@ void BinaryTree::insert_helper(TreeNode* root, int value) {
     }
 }
 
-void BinaryTree::inorder_traversal_helper(TreeNode* root) const {
-    if(root != nullptr) {
-        inorder_traversal_helper(root->left_);
-        printf("%d ", root->value_);
-        inorder_traversal_helper(root->right_);
+void BinaryTree::midorderTraversal() const {
+    midorderTraversalHelper(root);
+    printf("\n");
+}
+
+void BinaryTree::midorderTraversalHelper(TreeNode* rootOfTree) const {
+    if(rootOfTree != nullptr) {
+        midorderTraversalHelper(rootOfTree->left);
+        printf("%d ", rootOfTree->value);
+        midorderTraversalHelper(rootOfTree->right);
     }
 }
 
 int main()
 {
-    BinaryTree tree;
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(7);
-    tree.insert(2);
-    tree.insert(4);
-    tree.insert(6);
-    tree.insert(8);
-    tree.inorder_traversal();
+    BinaryTree* tree = new BinaryTree();
+    tree->insertNode(5);
+    tree->insertNode(3);
+    tree->insertNode(7);
+    tree->insertNode(2);
+    tree->insertNode(4);
+    tree->insertNode(6);
+    tree->insertNode(8);
+    tree->midorderTraversal();
+
+    delete tree;
+    tree = nullptr;
 }
+
