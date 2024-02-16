ae8e3843002344f68dd728afb26a0aed2e0fa8a9
niu0217 <1033856427@qq.com>
2024-02-16 20:19:26 +0800

    LeetCode15

--- Algorithm/HashTable/LeetCode15.cpp
+++ Algorithm/HashTable/LeetCode15.cpp
@@ -41,3 +41,47 @@ public:
         return result;
     }
 };
+
+//双指针
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(begin(nums), end(nums));
+        for(int i = 0; i < nums.size(); i++) {
+            //剪枝
+            if(nums[i] > 0) {
+                break;
+            }
+            //a + b + c=0
+            //对a去重
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while(left < right) {
+                if(nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                }
+                else if(nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                }
+                else {
+                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    //对b和c去重
+                    while(left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while(left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    //移动指针收割下一个结果
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+};
