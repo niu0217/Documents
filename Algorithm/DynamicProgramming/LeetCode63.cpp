e85eea02118b1b97200df20896832ba9bb6b1413
niu0217 <1033856427@qq.com>
2024-03-15 14:04:32 +0800

    动态规划：分割等和子集

--- Algorithm/DynamicProgramming/LeetCode63.cpp
+++ Algorithm/DynamicProgramming/LeetCode63.cpp
@@ -71,3 +71,28 @@ public:
         return dp[row - 1][col - 1];
     }
 };
+
+//再次简化
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int row = obstacleGrid.size();
+        int col = obstacleGrid[0].size();
+        vector<vector<int>> dp(row, vector<int>(col, 0));
+        for(int i = 0; i < row && obstacleGrid[i][0] != 1; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < col && obstacleGrid[0][j] != 1; j++) {
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < row; i++) {
+            for(int j = 1; j < col; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+};
